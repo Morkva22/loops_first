1cfd41efc2ee3294ef500b11d626a4969ac72911
Mark Maichenko <marcusmay0202@gmail.com>
2024-12-11 14:43:52 +0200

    d

--- loops.cpp
+++ loops.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <iostream>Користувач із клавіатури вводить числа. Порахувати їхню суму і вивести на
+екран, щойно користувач введе нуль.
 using namespace std;
 
 int main() {
