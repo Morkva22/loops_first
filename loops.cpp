387ef6dbb36080ee6936723719bdc1dccdb1c9a3
Mark Maichenko <marcusmay0202@gmail.com>
2024-12-11 14:41:49 +0200

    donex3

--- loops.cpp
+++ loops.cpp
@@ -1,12 +1,19 @@
-#include <iostream>
+#include <iostream> Користувач вводить дві межі діапазону. Порахувати суму всіх чисел діапазону.
 using namespace std;
 
 int main() {
-    
+    int a;
+    int b;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    int sum = 0;
+    for (int i = a; i <= b; i++) {
+        sum += i;
+    }
+    cout << "Sum: " << sum << endl;
 }
-
-
-
 
 
 
